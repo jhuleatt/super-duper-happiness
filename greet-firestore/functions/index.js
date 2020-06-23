a3189b3e10d8d3844a4e34924aae881a4eb1b542
Lauren Long <laurenzlong@users.noreply.github.com>
2020-06-23 13:58:41 -0700

    Remove unnecessary firebase-admin

--- greet-firestore/functions/index.js
+++ greet-firestore/functions/index.js
@@ -1,7 +1,4 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-
-admin.initializeApp();
 
 exports.greetFirestore = functions.handler.firestore.document.onWrite(
   async (change) => {
