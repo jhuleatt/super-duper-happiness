426a7c847d9ee7cd637633b4e0da994311f51189
Lauren Long <laurenzlong@users.noreply.github.com>
2020-06-23 13:59:39 -0700

    Remove firebase-admin

--- nasa-asteroid/functions/index.js
+++ nasa-asteroid/functions/index.js
@@ -1,8 +1,5 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
 const fetch = require('node-fetch');
-
-admin.initializeApp();
 
 exports.checkIfAsteroidIsDangerous = functions.handler.firestore.document.onWrite(
   async (change) => {
